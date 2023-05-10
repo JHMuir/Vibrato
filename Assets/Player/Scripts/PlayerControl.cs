075b80fbe6277cc2192b8108b2815cc8fa007eff
Jordan Muir <jordanhmuir@gmail.com>
2023-05-09 17:00:31 -0700

    Created Player Character
    created player character and added movement script, moved player sprites and script into a new folder

---
+++ Assets/Player/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour{
+
+    private Rigidbody2D body2D;
+    private SpriteRenderer sprite;
+
+    [Header("Stats")]
+    public float speed = 100.0f;
+    public float airControl = 0.5f;
+    public float jumpPower = 100.0f;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        body2D = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Move();
+    }
+
+    void Move(){
+        float horizontalMove = Input.GetAxis("Horizontal");
+
+        float moveForce = horizontalMove * speed * Time.deltaTime;
+
+        body2D.AddForce(Vector2.right * moveForce, ForceMode2D.Impulse);
+
+        sprite.flipX = horizontalMove < 0;
+    }
+}
