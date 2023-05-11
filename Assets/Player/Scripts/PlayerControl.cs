82f644b831e7c93fbd86a439941d52fe752c8d33
Jordan Muir <jordanhmuir@gmail.com>
2023-05-11 12:36:38 -0700

    Added player jump ability
    jump is very floaty, might refactor later

--- Assets/Player/Scripts/PlayerControl.cs
+++ Assets/Player/Scripts/PlayerControl.cs
@@ -9,8 +9,11 @@ public class PlayerControl : MonoBehaviour{
 
     [Header("Stats")]
     public float speed = 25.0f;
-    public float airControl = 0.5f;
-    public float jumpPower = 100.0f;
+    public float airControl = 0.8f;
+    public float jumpPower = 8.0f;
+
+    [Header("States")]
+    public bool inAir = false;
 
 
     // Start is called before the first frame update
@@ -22,21 +25,41 @@ public class PlayerControl : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         Move();
+        Jump();
+        
     }
 
     void Move(){
-        Vector2 horizontalMove = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //Tracking which direction the player would like to go
+        Vector2 horizontalMove = new Vector2(Input.GetAxis("Horizontal"), 0);
 
+        //Checking if the player is in the air
+        if(inAir){
+            horizontalMove *= airControl;
+        }
+
+        //Moving the player
         body2D.position += horizontalMove * speed * Time.deltaTime;
-        //float moveForce = horizontalMove * speed * Time.deltaTime;
 
-        //body2D.AddForce(Vector2.right * moveForce, ForceMode2D.Impulse);
-
+        //Making sure the player sprite is facing the right way
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
             sprite.flipX = true;
         }
         if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
             sprite.flipX = false;
         }
+    }
+
+    void Jump(){
+        //checking player input
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && !inAir){
+            //making the playre jump
+            body2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            inAir = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        inAir = false;
     }
 }
